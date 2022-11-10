329417379e6de7d1238a07067a83f2f8f857dbe3
Jean Leles <jeanleles.sa@gmail.com>
2022-11-09 23:57:06 -0300

    Corrgindo a tipagem do Typescript...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import { useState } from 'react'
 import Head from 'next/head'
 import { Aside } from '../src/components/Aside'
 import { Footer } from '../src/components/Footer'
@@ -8,9 +9,48 @@ import { MenuTop } from '../src/components/MenuTop'
 
 import { GraphQLClient } from 'graphql-request'
 
+interface CarnesProps {
+  carnes: [
+    id: string,
+    title: string,
+    description: string,
+    price: string,
+  ]
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const hygraph = new GraphQLClient(
+    'https://api-sa-east-1.hygraph.com/v2/cl8kvkg2w263601ulgfni5j80/master'
+  );
+
+  const { carnes } = await hygraph.request(
+    `
+      {
+        carnes {
+          id
+          title
+          description
+          price
+          amountPeople
+          photo {
+            url
+          }
+        }
+      }
+    `
+  );
+
+  return {
+    props: {
+      carnes,
+    },
+  };
+}
 
 
-const Home = () => {
+
+const Home = ({ carnes }: CarnesProps) => {
+  console.log(carnes)
   return (
     <div className="flex min-h-screen flex-col items-center w-full">
       <Head>
@@ -25,14 +65,19 @@ const Home = () => {
         <div className='flex flex-col w-full'>
           <MenuTop />
           <h2 className='text-2xl text-white uppercase font-bold my-2'>Carnes</h2>
-          <ItemMenu />
-          <ItemMenu />
-          <ItemMenu />
-          <ItemMenu />
-          <ItemMenu />
-          <ItemMenu />
-          <ItemMenu />
-          <ItemMenu />
+          {            
+            carnes.map((carne) => (
+              <div className='flex bg-[#252727] justify-between rounded-md mb-4 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] w-full'>
+                <div className='flex flex-col justify-around p-2 py-2 lg:p-4'>
+                  <h2 className='text-base font-bold text-slate-100 md:text-lg'>{carne.title}</h2>
+                  <p className='text-xs text-slate-300 md:text-base'>{carne.description}</p>
+                  <h2 className='text-sm md:text-lg  text-[#FAE374]'>{carne.price}</h2>
+                </div>
+                <img src="/CSE01.jpg" alt="carne de sol" className='w-[140px] rounded-tr-lg rounded-br-lg' />
+              </div>
+            ))
+          }
+
         </div>
       </main>
 
